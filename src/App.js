850771a1463924187addb897d14166e82de5815c
lcu1903 <92511021+lcu1903@users.noreply.github.com>
2022-12-29 22:45:29 +0700

    62

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRouter } from './routes';
+import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
 
 function App() {
     return (
-        <div className="App">
-            <h1> REACT APP</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRouter.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        const Page = route.component;
+                        
+                        if (route.layout) {
+                            Layout = route.layout;
+                        }else if(route.layout===null){
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+//PublicRoutes
+import Home from 'src/pages/Home';
+import Following from 'src/pages/Following';
+import Profile from 'src/pages/Profile';
+import Upload from 'src/pages/Upload';
+import Search from 'src/pages/Search';
+import { HeaderOnly } from 'src/components/Layout';
+
+
+const publicRouter = [
+    { path: '/', component: Home },
+    { path: '/home', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRouter = [];
+
+export { publicRouter, privateRouter };
